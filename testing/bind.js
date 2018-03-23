936611ad2f28f26238e1582f93333586a4895882
Ben Styles <enquiries@leeds-ebooks.co.uk>
2018-03-23 12:16:15 +0000

    add travis

--- testing/bind.js
+++ testing/bind.js
@@ -1,3 +1,10 @@
 const fn = () => null
 // eslint-disable-next-line no-restricted-syntax
 exports.fn = fn.bind(null, 1)
+
+const obj = {
+  method() {
+    return null
+  },
+}
+exports.method = obj.method.bind(obj)
